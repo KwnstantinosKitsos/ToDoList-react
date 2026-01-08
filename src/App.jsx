7208f9899d7294458e206934d7004c8d5bb9fcd3
Kwstas <ntinoskitsios1@gmail.com>
2026-01-08 17:19:44 +0200

    Complete todo app with edit task, add task, delet task, clear all, checked task, completed task and persistent storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import TaskForm from './TaskForm';
 import Task from './Task';
 import { useEffect, useState } from 'react';
+import confetti from 'canvas-confetti';
 
 function App() {
   //LIFTING THE STATE UP
@@ -17,13 +18,69 @@ function App() {
     localStorage.setItem('allTasks', JSON.stringify(allTasks));
   }, [allTasks]);
 
+  function updateTaskDone(indexTask, newDone) {
+    setAllTasks((prev) => {
+      const newTask = [...prev];
+      newTask[indexTask].done = newDone;
+      return newTask;
+    });
+  }
+
+  const completeNumber = allTasks.filter((a) => a.done).length;
+  const totalNumber = allTasks.length;
+
+  useEffect(() => {
+    if (allTasks.length > 0 && completeNumber === totalNumber) {
+      confetti({
+        particleCount: 150,
+        spread: 70,
+        origin: { y: 0.6 },
+      });
+    }
+  }, [completeNumber, totalNumber, allTasks.length]);
+
+  function removeTask(indexToRemove) {
+    setAllTasks((prev) => prev.filter((_, index) => index !== indexToRemove));
+  }
+
+  function renameTask(index, newName) {
+    setAllTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    });
+  }
+  function clearAllTasks() {
+    const confirmed = window.confirm('You want to clear all the Tasks?');
+    if (confirmed) {
+      setAllTasks([]);
+    }
+  }
   return (
     <main className="main-component">
       <TaskForm onAdd={addTask} />
-      {allTasks.map((allTask) => (
+
+      {allTasks.map((allTask, index) => (
         //spread ops (name={allTask.name}, done={allTask.done})
-        <Task {...allTask} />
+        <Task
+          {...allTask}
+          onDelete={() => removeTask(index)}
+          onToggle={(done) => updateTaskDone(index, done)}
+          onRename={(newName) => renameTask(index, newName)}
+        />
       ))}
+      {allTasks.length > 0 && (
+        <button className="btn-clear" onClick={clearAllTasks}>
+          Clear All Tasks
+        </button>
+      )}
+
+      <h3 className="myH3">
+        {allTasks.length > 0 && completeNumber === totalNumber && (
+          <h1 className="congrats-msg">🎉 Congrats! All tasks done! 🎉</h1>
+        )}
+        {completeNumber}/{totalNumber} Complete
+      </h3>
     </main>
   );
 }
