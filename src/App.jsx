13ae7b3bd7159af6f221d3d1a29ed776b18105c7
Kwstas <ntinoskitsios1@gmail.com>
2026-01-08 15:00:05 +0200

    Save and load tasks from localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import './App.css';
 import TaskForm from './TaskForm';
 import Task from './Task';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   //LIFTING THE STATE UP
-  const [allTasks, setAllTasks] = useState([]);
+  const [allTasks, setAllTasks] = useState(() => {
+    //
+    const saved = localStorage.getItem('allTasks');
+    return saved ? JSON.parse(saved) : [];
+  });
   function addTask(name) {
     setAllTasks((a) => [...a, { name: name, done: false }]);
   }
+  useEffect(() => {
+    localStorage.setItem('allTasks', JSON.stringify(allTasks));
+  }, [allTasks]);
+
   return (
     <main className="main-component">
       <TaskForm onAdd={addTask} />
       {allTasks.map((allTask) => (
+        //spread ops (name={allTask.name}, done={allTask.done})
         <Task {...allTask} />
       ))}
     </main>
